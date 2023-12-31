8246117573673a796d1e26bc3ea4c1f38c294e95
Will Johnston <will.johnston@kroger.com>
2023-12-31 12:58:47 -0800

    feat: configures eslint, prettier, lint-staged

---
+++ apps/api/hmm-api/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    rules: {
+      // Add any project-specific rules or overrides here
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    overrides: [
+      {
+        files: ['*.test.ts'], // Adjust this pattern based on your test file naming convention
+        env: {
+          jest: true,
+        },
+      },
+    ],
+  };
+  
