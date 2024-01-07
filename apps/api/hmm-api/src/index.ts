a58d515a0715fe3cd1763aa6d3cc2ab1934ecb3b
Will Johnston <will.johnston@kroger.com>
2024-01-07 13:50:48 -0800

    feat: adds graceful exit

--- apps/api/hmm-api/src/index.ts
+++ apps/api/hmm-api/src/index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import config from 'config'
+import mongoose from 'mongoose'
+import Graceful from '@ladjs/graceful'
 
 import { dbProvider } from './providers/db-provider/db-provider'
 
@@ -7,7 +9,14 @@ const app = new Koa()
 
 const port = config.get('PORT') as number
 
-app.listen(port, () => {
+const hmmApiServer = app.listen(port, () => {
   console.log(`\nHMM-API is listening on port ${port} \n`)
   dbProvider.init()
 })
+
+const graceful = new Graceful({
+  servers: [hmmApiServer],
+  mongooses: [mongoose],
+  logger: console,
+})
+graceful.listen()
